9fc236214653e6379562f0f9860848e6b5b863d8
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-10 20:27:20 -0600

    update HistorialAcademico.java

--- src/java/buap/intro_programacion/models/HistorialAcademico.java
+++ src/java/buap/intro_programacion/models/HistorialAcademico.java
@@ -8,5 +8,42 @@ public class HistorialAcademico {
     private Curso[] cursos;
     private String[] competencias;
 
+    public HistorialAcademico(Estudiante estudiante, Grado grado, Curso[] cursos, String[] competencias) {
+        this.estudiante = estudiante;
+        this.grado = grado;
+        this.cursos = cursos;
+        this.competencias = competencias;
+    }
 
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Grado getGrado() {
+        return grado;
+    }
+
+    public void setGrado(Grado grado) {
+        this.grado = grado;
+    }
+
+    public Curso[] getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(Curso[] cursos) {
+        this.cursos = cursos;
+    }
+
+    public String[] getCompetencias() {
+        return competencias;
+    }
+
+    public void setCompetencias(String[] competencias) {
+        this.competencias = competencias;
+    }
 }
