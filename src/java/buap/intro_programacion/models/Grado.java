dd7a82906ca6ca60ddf1debb5a806d334640effc
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-10 20:10:27 -0600

    utilizar array en Grado.java

--- src/java/buap/intro_programacion/models/Grado.java
+++ src/java/buap/intro_programacion/models/Grado.java
@@ -2,5 +2,5 @@ package buap.intro_programacion.models;
 
 public class Grado {
     private String id;
-    private java.lang.reflect.Array lista;
+    private  String[] lista={"Lunes", "Martes"};
 }
