b305224e2bcc06c20839435d10106b1948145892
PoloBustillo <leopoldobeuiluz1@hotmail.com>
2023-10-10 20:49:17 -0600

    method to center jframe

--- src/java/buap/intro_programacion/SistemaEscolarUI.java
+++ src/java/buap/intro_programacion/SistemaEscolarUI.java
@@ -5,13 +5,22 @@ import java.awt.*;
 //TODOS
 public class SistemaEscolarUI extends JFrame {
 
-    private JPanel Hola;
-    private JButton XD;
+    private JPanel mainPanel;
 
+    private void centerFrame(JFrame frame){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        setSize(new Dimension((int) (width-200), (int) (height-200)));
+        int x = (int) ((screenSize.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((screenSize.getHeight() - frame.getHeight()) / 2);
+        setLocation(x,y);
+    }
     public SistemaEscolarUI(String title) throws HeadlessException {
         super(title);
+        centerFrame(this);
         setVisible(true);
-        setSize(700,500);
+
     }
 
     public static void main(String[] args) {
