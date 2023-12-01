9cc3419a11c2ee35a99fcfb940529d82075c33b3
Jorgekkk <142845784+Jorgekkk@users.noreply.github.com>
2023-11-30 22:15:35 -0600

    x
    s

--- src/java/main/buap/intro_programacion/models/Direccion.java
+++ src/java/main/buap/intro_programacion/models/Direccion.java
@@ -6,6 +6,42 @@ public class Direccion {
     private Integer codigoPostal;
     private String ciudad;
 
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public Integer getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(Integer codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Direccion(String calle) {
+        this.calle = calle;
+    }
+
     /**
      * @param calle
      * @param colonia
